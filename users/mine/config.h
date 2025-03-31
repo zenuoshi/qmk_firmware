b43351dc3c2b1cda1db69a94b1128a9b4f2a19df
Zenuoshi <zenuoshi@gmail.com>
2025-03-31 21:51:15 +0900

    Adds custom hotdox keymap with layers and combo support
    Introduces a personalized keymap for the hotdox keyboard, including multiple layers like Dvorak, QWERTY, gaming, and system controls. Implements combo key functionality for modifiers and custom actions. Adds random input generators and utility functions. Configures LED indicators for layer states.
    
    Improves modularity with separate user-specific configuration and processing files.

---
+++ users/mine/config.h
@@ -0,0 +1,3 @@
+#pragma once
+
+// #define USB_POLLING_INTERVAL_MS 1
---
+++ users/mine/process_records.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "mine.h"
+
+enum userspace_custom_keycodes {
+    PLACE_HOLDER = SAFE_RANGE,
+    KC_RANDOM,
+    KC_VERSION,
+    KC_LSFT_F13,
+    KC_RSFT_F14,
+    KC_LNBRK,
+    NEW_SAFE_RANGE,
+};
