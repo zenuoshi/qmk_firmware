7e6fff3eb4b7b917b3590f61bab42a2c24b8a53e
Zenuoshi <zenuoshi@gmail.com>
2025-04-02 13:01:52 +0900

    feat(hotdox): activate QMK_KEYS_PER_SCAN macro
    Enable the QMK_KEYS_PER_SCAN macro in the configuration to optimize key scanning performance.

--- keyboards/hotdox/keymaps/zenuoshi/config.h
+++ keyboards/hotdox/keymaps/zenuoshi/config.h
@@ -3,9 +3,9 @@
 // #undef DEBOUNCE
 // #define DEBOUNCE 20
 
-// #ifndef QMK_KEYS_PER_SCAN
-// #    define QMK_KEYS_PER_SCAN 4
-// #endif
+#ifndef QMK_KEYS_PER_SCAN
+#    define QMK_KEYS_PER_SCAN 4
+#endif
 
 // USER DEFINE
 #define ENABLE_MY_COMBO
